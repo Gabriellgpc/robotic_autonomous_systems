3d5315bb32cf6531fecda55d5f745932c1b0f94d
Luís Gabriel <gabriellgpc@hotmail.com>
2020-09-22 10:59:56 -0300

    upgrade

--- projeto/meta1.py
+++ projeto/meta1.py
@@ -28,12 +28,12 @@ import sys
 #################################### Functions definition ############################################## 
 # calcula as velocidades que devem ser aplicadas para realizar um circulo de radio R no tempo t
 # retorna Wd, We
-def omegas(b, r, R, delta_t):
-    w = ((2.0*np.pi*R)/delta_t) * r
-    we = ((R - b/2.0)/r) * w
-    wd = we * (R+b/2.0)/(R - b/2.0)
-
-    print('Omega = {} | Omega_D = {} | Omega_E = {}'.format(w, wd,we))
+def omegas(b, r, R, W):
+    wd = W*(R + b/2.0)/r
+    # wd = W/(r*(R + b/2.0))
+    we = wd * (R - b/2.0)/(R+b/2.0)
+    print('Omega = {} | Omega_D = {} | Omega_E = {} | V = {}'.format(W, wd,we, r*(wd+we)/2.0))
+    print('V_desejado = {} | V_obtido = {}'.format(W*R, (wd*r + we*r)/2.0))
     return wd,we
 ##################################### Connecting to simulator ##################################################
 
@@ -69,12 +69,12 @@ ax_xy.set_ylim([-0.5,4.5])
 ax_xy.set_xlabel('x[m]')
 ax_xy.set_ylabel('y[m]')
 ax_xy.grid(False)
-ax_xy.plot([],[], '*k')
+ax_xy.plot([],[], 'k*')
 
 fig2, (ax_th, ax_w) = plt.subplots(2, 1, constrained_layout=True, sharex=True)
 
 ax_th.set_ylabel(r'$\theta$ [rad]')
-ax_th.plot([], [], '-r', label=r'$\theta$')
+ax_th.plot([], [], '-b', label=r'$\theta$')
 ax_th.set_ylim([-np.pi, np.pi])
 ax_th.legend()
 ax_th.grid(True)
@@ -89,16 +89,18 @@ ax_w.legend()
 plt.ion()
 ######################################## simulation  #######################################
 b = 0.331 #wheel axis distance [m]
-r = 0.1 #wheel radius [m]
+r = 0.09751#wheel radius [m]
 R = 2.0 #[m]
-deltaT = 20.0 #[s]
-wd,we = omegas(b,r,R, deltaT)
+deltaT = 30.0 #[s]
+W = (2*np.pi)/deltaT
+wd,we = omegas(b, r, R, W)
 
-stopTime = 20.0 #[s]
+stopTime = 30.0 #[s]
 begin = time.time() #[s]
 t = 0
 while t <= stopTime:
     t = time.time() - begin
+    
     # getting absolute position and orientation(euler) information
     resA,ang = sim.simxGetObjectOrientation(clientID, robot_handle, -1, sim.simx_opmode_streaming)    
     resP,pos = sim.simxGetObjectPosition(clientID,robot_handle,-1, sim.simx_opmode_streaming)
@@ -115,7 +117,7 @@ while t <= stopTime:
     th_vec.append(ang[2])
 
     #live plotting
-    ax_xy.plot(x_vec,y_vec,'*k')
+    ax_xy.plot(x_vec,y_vec,'k*')
     ax_th.plot(t_vec, th_vec, '-b')
     ax_w.plot(t_vec, wd_vec, '-r', t_vec, we_vec, '-g')
     plt.pause(0.01)
@@ -124,7 +126,6 @@ while t <= stopTime:
     sim.simxSetJointTargetVelocity(clientID,RwMotor_handle,wd,sim.simx_opmode_streaming)
     sim.simxSetJointTargetVelocity(clientID,LwMotor_handle,we,sim.simx_opmode_streaming)
 
-    time.sleep(0.1)
 end = time.time()
 print('Tempo Decorrido:', end - begin)
 
@@ -139,7 +140,7 @@ sim.simxFinish(clientID)
 
 ######################################## Static plotting  #######################################
 plt.ioff()
-ax_xy.plot(x_vec,y_vec,'-ok')
+ax_xy.plot(x_vec,y_vec,'k*')
 ax_th.plot(t_vec, th_vec, '-b')
 ax_w.plot(t_vec, wd_vec, '-r', t_vec, we_vec, '-g')
 plt.show()
