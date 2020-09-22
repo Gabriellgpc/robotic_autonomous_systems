66fa0ed0f253a680a18bf34012138cd05a999fdf
Luís Gabriel <gabriellgpc@hotmail.com>
2020-09-21 23:46:46 -0300

    update- Realiza percurso circular com base no modelo cinemático

--- projeto/meta1.py
+++ projeto/meta1.py
@@ -26,8 +26,15 @@ import numpy as np
 import sys
 
 #################################### Functions definition ############################################## 
-def radius_to_speeds(radius, b, r):
-    pass
+# calcula as velocidades que devem ser aplicadas para realizar um circulo de radio R no tempo t
+# retorna Wd, We
+def omegas(b, r, R, delta_t):
+    w = ((2.0*np.pi*R)/delta_t) * r
+    we = ((R - b/2.0)/r) * w
+    wd = we * (R+b/2.0)/(R - b/2.0)
+
+    print('Omega = {} | Omega_D = {} | Omega_E = {}'.format(w, wd,we))
+    return wd,we
 ##################################### Connecting to simulator ##################################################
 
 clientID = sim.simxStart('127.0.0.1',19999,True,True,5000,5)
@@ -57,17 +64,18 @@ th_vec = list()
 ####################################### graph configuration #######################################
 fig1 = plt.figure(figsize=(4, 4))
 ax_xy = fig1.add_subplot(1, 1, 1, aspect=1)
-ax_xy.set_xlim([-4,4])
-ax_xy.set_ylim([-4,4])
+ax_xy.set_xlim([-2.5,2.5])
+ax_xy.set_ylim([-0.5,4.5])
 ax_xy.set_xlabel('x[m]')
 ax_xy.set_ylabel('y[m]')
 ax_xy.grid(False)
-ax_xy.plot([],[], '*-k')
+ax_xy.plot([],[], '*k')
 
 fig2, (ax_th, ax_w) = plt.subplots(2, 1, constrained_layout=True, sharex=True)
 
 ax_th.set_ylabel(r'$\theta$ [rad]')
 ax_th.plot([], [], '-r', label=r'$\theta$')
+ax_th.set_ylim([-np.pi, np.pi])
 ax_th.legend()
 ax_th.grid(True)
 
@@ -80,13 +88,16 @@ ax_w.legend()
 
 plt.ion()
 ######################################## simulation  #######################################
-stopTime = 30.0
-begin = time.time()
+b = 0.331 #wheel axis distance [m]
+r = 0.1 #wheel radius [m]
+R = 2.0 #[m]
+deltaT = 20.0 #[s]
+wd,we = omegas(b,r,R, deltaT)
+
+stopTime = 20.0 #[s]
+begin = time.time() #[s]
 t = 0
 while t <= stopTime:
-    we = 4.0
-    wd = we*0.9
-
     t = time.time() - begin
     # getting absolute position and orientation(euler) information
     resA,ang = sim.simxGetObjectOrientation(clientID, robot_handle, -1, sim.simx_opmode_streaming)    
@@ -104,7 +115,7 @@ while t <= stopTime:
     th_vec.append(ang[2])
 
     #live plotting
-    ax_xy.plot(x_vec,y_vec,'*-k')
+    ax_xy.plot(x_vec,y_vec,'*k')
     ax_th.plot(t_vec, th_vec, '-b')
     ax_w.plot(t_vec, wd_vec, '-r', t_vec, we_vec, '-g')
     plt.pause(0.01)
@@ -119,8 +130,10 @@ print('Tempo Decorrido:', end - begin)
 
 sim.simxSetJointTargetVelocity(clientID,RwMotor_handle,0.0,sim.simx_opmode_streaming)
 sim.simxSetJointTargetVelocity(clientID,LwMotor_handle,0.0,sim.simx_opmode_streaming)
+
 # Before closing the connection to CoppeliaSim, make sure that the last command sent out had time to arrive. You can guarantee this with (for example):
 sim.simxGetPingTime(clientID)
+
 # Now close the connection to CoppeliaSim:
 sim.simxFinish(clientID)
 
