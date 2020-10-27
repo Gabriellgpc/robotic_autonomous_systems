ff11390e2f7e10ab8c8983c866f43892f35a1609
Gabriel <gabriellgpc@hotmail.com>
2020-10-27 20:26:08 -0300

    inicio das implementações do configSpaceTools

--- source/utils/configSpaceTools.cpp
+++ source/utils/configSpaceTools.cpp
@@ -1,34 +1,147 @@
 #include "configSpaceTools.hpp"
 #include <cmath>
+#include <tuple>     //tuple
+#include <algorithm> //find
 
-typedef std::list<Coord2D>::const_iterator vertex_it;
+// Vector2D
+
+// Vector2D
+
+// Polygon2D
+typedef std::list<Vector2D>::const_iterator vertex_it;
+
+std::list<Vector2D> Polygon2D::get_vertices() const
+{
+    return this->my_vertices;
+}
+
+std::list<Vector2D> Polygon2D::get_normalVectors() const
+{
+    std::list<Vector2D> normals;
+    vertex_it it = this->my_vertices.begin();
+    Vector2D p[2]; //point/vertex p_k and p_k+1
+    Vector2D normal_tmp;
+
+    if (Polygon2D::check_convexity(*this) == false)
+        return normals;
+
+    while (it != this->my_vertices.end())
+    {
+        p[0] = *it;
+        ++it;
+        if (it == this->my_vertices.end())
+            p[1] = *this->my_vertices.begin();
+        else
+            p[1] = *it;
+
+        //vetor paralelo ao lado ai ai+1
+        normal_tmp = Vector2D(p[1]) - Vector2D(p[0]);
+        //vetor normal ao lado ai ai+1
+        normal_tmp = Vector2D(normal_tmp.y(), -normal_tmp.x());
+        //normalizando o vetor
+        normal_tmp.normalize();
+        //adicionado a lista de normais
+        normals.push_back(normal_tmp);
+    }
+    return normals;
+}
+
+bool operator<(std::tuple<Vector2D, Vector2D, bool> A, std::tuple<Vector2D, Vector2D, int> B)
+{
+    return (std::get<1>(A)).ang() < (std::get<1>(B)).ang();
+}
+
+Polygon2D Polygon2D::work_to_config_space(const Robot &robot) const
+{
+    Polygon2D polygon;
+    std::list<Vector2D> ob_normals;
+    std::list<Vector2D> robot_normals;
+    std::list<Vector2D> robot_vertices;
+    //tupla contando os vetores normais e os respectivos vertices
+    std::list<std::tuple<Vector2D, Vector2D, bool>> normal_vertex_list;
+
+    ob_normals = this->get_normalVectors();
+    robot_normals = robot.my_shape.get_normalVectors();
+    robot_vertices = robot.my_shape.get_vertices();
+
+    //preenchendo a tupla normal_vertex_list
+
+    //dados relativos ao objeto/obstaculo (indicado pelo numero/flag 0/false)
+    std::list<Vector2D>::const_iterator it_normal = ob_normals.begin();
+    std::list<Vector2D>::const_iterator it_vertex = this->my_vertices.begin();
+    for (int i = 0; i < ob_normals.size(); i++)
+    {
+        auto normal_vertex = std::make_tuple(*(it_vertex++), *(it_normal++), false);
+        normal_vertex_list.push_back(normal_vertex);
+    }
+
+    //dados relativos ao robo (indicado pelo numero/flag 1)
+    it_normal = robot_normals.begin();
+    it_vertex = robot_vertices.begin();
+    for (int i = 0; i < robot_normals.size(); i++)
+    {
+        //empacotando o vertice com a respectiva normal negativada (vertex, -normal, 1/true(flag::robot))
+        auto normal_vertex = std::make_tuple(*(it_vertex++), -*(it_normal++), true);
+        normal_vertex_list.push_back(normal_vertex);
+    }
+    //sorting normal_vertex list according to normal vector
+    normal_vertex_list.sort();
+    //percorrer a lista ordenada e calcular os vertices do C-obstaculo
+    auto it_curr = normal_vertex_list.begin();
+    //a: vertice do robo no referencial do robo
+    //b: vertice do objeto no referencial do mundo
+    Vector2D a, b;
+    while (it_curr != normal_vertex_list.end())
+    {
+        bool is_robot = std::get<2>(*it_curr);
+        auto it_next = std::find(it_curr, normal_vertex_list.end(), !is_robot);
+
+        if (is_robot)
+        {
+            a = robot.my_config.my_pos - std::get<0>(*it_curr);
+            b = std::get<0>(*it_next);
+        }
+        else
+        {
+            a = robot.my_config.my_pos - std::get<0>(*it_next);
+            b = std::get<0>(*it_curr);
+        }
+        polygon.add_vertex(b-a);
+    }
+
+    return polygon;
+}
 
 bool Polygon2D::check_convexity(const Polygon2D &poly)
 {
-    if(poly.vertices.size() < 3)return false;
+    if (poly.my_vertices.size() < 3)
+        return false;
 
-    vertex_it v_it = poly.vertices.begin();
-    double a,b,c,d;
-    Coord2D pk[2];
-    
-    while(v_it != poly.vertices.end()){
+    vertex_it v_it = poly.my_vertices.begin();
+    double a, b, c, d;
+    Vector2D pk[2];
+
+    while (v_it != poly.my_vertices.end())
+    {
         pk[0] = *v_it;
-        v_it++;    
-        if(v_it == poly.vertices.end())
-            pk[1] = *poly.vertices.begin();
+        v_it++;
+        if (v_it == poly.my_vertices.end())
+            pk[1] = *poly.my_vertices.begin();
         else
             pk[1] = *v_it;
         //reta que vai de p0 ate p1
-        a = pk[0].y - pk[1].y;
-        b = pk[1].x - pk[0].x;
-        c = pk[0].x*pk[1].y - pk[1].x*pk[0].x;
-        //verifica se algum dos vertices encontra-se no semi plano direito dessa reta (d < 0)
-        //Obs.: poligono com vertices numerados de forma ordenada no sentido anti-horário
-        for(vertex_it it = poly.vertices.begin(); it != poly.vertices.end(); it++){
-            Coord2D p(*it);
-            d = (a*p.x + b*p.y + c)/sqrt(a*a + b*b);
-            if(d < 0.0)return false;
-        }    
+        a = pk[0].y() - pk[1].y();
+        b = pk[1].x() - pk[0].x();
+        c = pk[0].x() * pk[1].y() - pk[1].x() * pk[0].x();
+        //verifica se algum dos my_vertices encontra-se no semi plano direito dessa reta (d < 0)
+        //Obs.: poligono com my_vertices numerados de forma ordenada no sentido anti-horário
+        for (vertex_it it = poly.my_vertices.begin(); it != poly.my_vertices.end(); it++)
+        {
+            Vector2D p(*it);
+            d = (a * p.x() + b * p.y() + c) / sqrt(a * a + b * b);
+            if (d < 0.0)
+                return false;
+        }
     }
     return true;
 }
--- source/utils/configSpaceTools.hpp
+++ source/utils/configSpaceTools.hpp
@@ -1,40 +1,35 @@
 #pragma once
 #include <list>
-#include <vector>
+#include <tuple>
 //Todas as unidades de medidas no padrão SI
-
-class Coord2D{
-public:
-    Coord2D():x(0.0), y(0.0){}
-    Coord2D(double _x, double _y):x(_x),y(_y){}
-
-    bool operator==(const Coord2D &other);
-
-    double x;
-    double y;
-};
 
 class Vector2D{
 public:
     Vector2D();
     Vector2D(const double _x, const double _y);
-    Vector2D(const Coord2D &point);
+    Vector2D(const Vector2D &other);
 
+    Vector2D operator-()const;
     Vector2D operator-(const Vector2D &other)const;
     Vector2D operator+(const Vector2D &other)const;
+    Vector2D operator*(const double &k)const;
+    
     bool operator==(const Vector2D &other)const;
 
     double x()const;
     double y()const;
     double norm()const;
     double dot(const Vector2D &other)const;
+    //retorna o angulo (theta) do vetor com relação ao eixo x positivo. theta in [0, 2pi]
+    double ang()const;
     
     void operator=(const Vector2D &other);
     void normalize();
-    void translate(const Coord2D &trans); //[m]
+    void translate(const Vector2D &trans); //[m]
     void rotation(const double &theta);   //[rad]
 private:
-    Coord2D vec;
+    double x;
+    double y;
 };
 
 class Polygon2D{
@@ -42,22 +37,49 @@ public:
     Polygon2D();
     ~Polygon2D();
     
-    Polygon2D(std::list<Coord2D> _vertices);
-    void add_vertex(const Coord2D _vertex);
+    Polygon2D(std::list<Vector2D> _vertices);
+    void add_vertex(const Vector2D _vertex);
+
+    Polygon2D work_to_config_space(const Robot &robot)const;
+    //retorna o poligono resultante da translação do atual poligono
+    Polygon2D translate(const double &x, const double &y)const; //[m]
+    Polygon2D translate(const Vector2D &trans)const; //[m]
+    //retorna o poligono resultante da rotação do atual poligono em theta radianos
+    Polygon2D rotation(const double &theta)const;   //[rad]
     
-    void translate(const double &x, const double &y); //[m]
-    void translate(const Coord2D &trans); //[m]
-    void rotation(const double &theta);   //[rad]
-    
-    double penetration_test(const Coord2D &p)const;
+    //retorna um vetor contendo Vector2D para cada normal face/lado do poligono
+    std::list<Vector2D> get_normalVectors()const;
+    std::list<Vector2D> get_vertices()const;
+
+    double penetration_test(const Vector2D &p)const;
+    //retorna true caso tenha sobreposição entre os poligonos (entre this e other)
     bool check_overlay(const Polygon2D other)const;
+
+    //operador de atribuição
+    void operator=(const Polygon2D &other);
 
     static bool check_convexity(const Polygon2D &poly);
     static Polygon2D circle_to_polygon2D(const double &radius);
     //p1: vertice inferior esquerdo
     //p2: vertice superior direito
-    static Polygon2D rectangle_to_polygon2D(const Coord2D &p1, const Coord2D &p2);
-    static Polygon2D rectangle_to_polygon2D(const Coord2D &center, const double &width, const double height);
+    static Polygon2D rectangle_to_polygon2D(const Vector2D &p1, const Vector2D &p2);
+    static Polygon2D rectangle_to_polygon2D(const Vector2D &center, const double &width, const double height);
 private:
-    std::list<Coord2D> vertices;
+    std::list<Vector2D> my_vertices;
+};
+
+class Config{
+public:
+    Config();
+
+    Vector2D my_pos;
+    double  my_theta;
+};
+
+class Robot{
+public:
+    Robot();
+
+    Polygon2D my_shape;
+    Config my_config;
 };
